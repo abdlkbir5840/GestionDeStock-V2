5cd7d640e73537796c90454d3aec136e0ace3280
abdlkbir5840 <habonbahmadi@gmail.com>
2023-04-12 02:50:16 +0100

    creating custom validators and handling exceptions

--- src/main/java/com/abdelkabir/dto/VenteDto.java
+++ src/main/java/com/abdelkabir/dto/VenteDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 public class VenteDto {
     private Integer id;
+
     @NotNull(message = "nom shouldn't be null")
     @NotBlank(message = "nom shouldn't be empty")
     private String code;
